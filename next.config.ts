a13de1866dbe10ca30fc5534cf04576935168162
Baaj <gurmaanvibe6@gmail.com>
2026-04-13 00:37:41 +0530

    fixed netlify error

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  turbopack: {
-    root: "/Users/gurmansingh/.gemini/antigravity/brain/cd94b6e3-ea4d-4ac6-ab1d-f0d90e4e0d51/browser/portfolio",
-  },
+  /* config options here */
 };
 
 export default nextConfig;
